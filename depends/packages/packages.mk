26bdd7984b1066f344a4f042405bfc2b21931cb9
Ian Blenke <ian@blenke.com>
2017-06-20 13:52:40 -0400

    Fixing packages for successful build

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,3 @@
-rust_packages := rust librustzcash
-proton_packages := proton
 zcash_packages := libsnark libgmp libsodium
 packages := boost openssl libevent zeromq $(zcash_packages)
 native_packages := native_ccache
